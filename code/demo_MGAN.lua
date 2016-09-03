f9cf8bdecbf397f91263f099a4d3a1938088e409
ravage-nx <nx.tardis@gmail.com>
2016-09-03 17:48:55 +0800

    update demo
    add camera model and remove some unnecessary code.

--- code/demo_MGAN.lua
+++ code/demo_MGAN.lua
@@ -5,6 +5,12 @@ require 'optim'
 require 'cutorch'
 require 'cunn'
 require 'image'
+
+--camera--
+require 'sys'
+require 'camera'
+require 'xlua'
+
 loadcaffe_wrap = paths.dofile('lib/loadcaffe_wrapper.lua')
 util = paths.dofile('lib/util.lua')
 pl = require('pl.import_into')()
@@ -12,9 +18,10 @@ pl = require('pl.import_into')()
 
 local opt = {}
 
-opt.model_name = '../Dataset/VG_Alpilles_ImageNet100/VG_Alpilles_ImageNet100_epoch5.t7'
+--opt.model_name = '../model/VG_Alpilles_ImageNet100_epoch20.t7'
+opt.model_name = '../model/Picasso.t7'
+--opt.model_name = '../model/Kandinsky_Complex.t7'
 opt.output_folder_name = '../Dataset/VG_Alpilles_ImageNet100/syn_VG_Alpilles_ImageNet100_epoch5/'
-opt.input_folder_name = '../Dataset/Testing_photo_ImageNet/' 
 
 opt.max_length = 512 -- change this value for image size. Larger images needs more gpu memory.
 opt.stand_atom = 8 -- make sure the image size can be divided by stand_atom
@@ -22,11 +29,25 @@ opt.noise_weight = 0.0 -- change this weight for balancing between style and con
 opt.noise_name = 'noise.jpg' -- low frequency noise image
 opt.gpu = 1
 
+-- camera
+dev = 0
+width = 640
+height = 480
+fps = 30
+
+
+camera1 = image.Camera{idx=dev, width=width, height=height, fps=fps}
+local image_input_ori = camera1:forward()
+local image_syn = camera1:forward()
+win1 = image.display{win=win1, image={image_input_ori}}
+win2 = image.display{win=win2, image={image_syn}}
+
+f = 1
+
 -- load data
-os.execute('mkdir ' .. opt.output_folder_name)
-local input_images_names = pl.dir.getallfiles(opt.input_folder_name, '*.png')
-local num_input_images = #input_images_names
+---------------
 local noise_image = image.load(opt.noise_name, 3)
+-------------
 local net = util.load(opt.model_name, opt.gpu)
 net:cuda()
 print(net)
@@ -35,9 +56,10 @@ print('*****************************************************')
 print('Testing: ');
 print('*****************************************************') 
 local counter = 0
-for i_img = 1, num_input_images  do
+while true  do
     -- resize the image image
-    local image_input = image.load(input_images_names[i_img], 3)
+    image_input_ori = camera1:forward()
+    local image_input = image_input_ori
     local max_dim = math.max(image_input:size()[2], image_input:size()[3])
     local scale = opt.max_length / max_dim
     local new_dim_x = math.floor((image_input:size()[3] * scale) / opt.stand_atom) * opt.stand_atom
@@ -53,22 +75,19 @@ for i_img = 1, num_input_images  do
 
     -- decode image with a single forward prop
 	local tm = torch.Timer()
-	local image_syn = net:forward(image_input)  
+	image_syn = net:forward(image_input)  
     cutorch.synchronize()
 	print(string.format('Image size: %d by %d, time: %f', image_input:size()[3], image_input:size()[4], tm:time().real))
     image_syn = image.toDisplayTensor{input = image_syn, nrow = math.ceil(math.sqrt(image_syn:size(1)))}
 
     -- save image
-    local image_name = input_images_names[i_img]:match("([^/]+)$")
-    image_name = string.sub(image_name, 1, string.len(image_name) - 4)
-    image.save(opt.output_folder_name .. image_name .. '_MGANs.jpg', image_syn)
-
-    -- clear memory
-    image_input = nil
-    image_syn = nil
-    noise_image_ = nil
-    collectgarbage()
+    image.display{win=win1, image={image_input_ori}}
+    image.display{win=win2, image={image_syn}}
+    
 end
 
+image_input = nil
+image_syn = nil
+noise_image_ = nil
 net = nil
 collectgarbage()
